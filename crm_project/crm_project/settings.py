00c894c0cd14d20b76f73cf0c36d6f294ae7cc69
Kartik <iamkaranmishra551@gmail.com>
2024-07-25 01:40:57 +0530

    Adding login and logout and much more functionality.

--- crm_project/crm_project/settings.py
+++ crm_project/crm_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from environ import Env
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#  To change message constants
+MESSAGE_TAGS = {
+
+    messages.ERROR: "danger"
+}
--- crm_project/website/views.py
+++ crm_project/website/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In.")
+            return redirect("home")
+        else:
+            messages.error(request, "There was an error UserName or Password.")
+            return redirect("home")
+
+    else:
+        return render(request, "website/index.html")
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "You're Successfully Logged Out.")
+    return redirect("home")
